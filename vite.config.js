345f235f87f2e002e085823e878345581ed89148
MgHtetMyatAung <htetmyat@innovix-solutions.com>
2024-11-14 16:03:07 +0630

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,30 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer({ open: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // Separate React and React-DOM into their own chunk
-            if (id.includes("react")) return "react-vendors";
-            // Separate other large dependencies
-            if (id.includes("lodash")) return "lodash-vendors";
-            // Catch-all for remaining node_modules
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  // resolve: {
+  //   alias: {
+  //     react: resolve("react"),
+  //     "react-dom": resolve("react-dom"),
+  //   },
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           // Separate React and React-DOM into their own chunk
+  //           if (id.includes("react")) return "react-vendors";
+  //           // Separate other large dependencies
+  //           if (id.includes("lodash")) return "lodash-vendors";
+  //           // Catch-all for remaining node_modules
+  //           return "vendor";
+  //         }
+  //       },
+  //     },
+  //   },
+  //   chunkSizeWarningLimit: 1000,
+  // },
+  // optimizeDeps: {
+  //   include: ["react", "react-dom"],
+  // },
 });
